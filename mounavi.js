5cb8601105f2d2ea18788392cdab4f79d72f79ff
SareddyMounavi <135025770+SareddyMounavi@users.noreply.github.com>
2023-05-31 09:45:07 +0530

    Add files via upload

---
+++ mounavi.js
@@ -0,0 +1,52 @@
+/*var name="mounavi"+"<br>"
+var age=19
+let name1="sareddy mounavi"+"<br>"
+let age1=19
+const clg="cbit"+"<br>"
+const mobile=9963641933
+const dob="19-08-2003"
+console.log(dob)
+document.write(clg)
+console.log(mobile)
+document.write(name1)
+console.log(age)*/
+/*var name="suhasini"
+function js(){
+    var age=20
+    document.write(name+"<br>")
+    document.write(age)
+}
+js()
+document.write(name)*/
+/*var std={
+    name:"mounavi",
+    rollno:475,
+    mail:"sareddeymounu2003@gmail.com"
+}
+document.write(std.mail +"<br>")
+document.write(std.name+"<br>")*/
+/*var std={
+    nam:"mounavi",
+    rollno:475,
+    mail:"sareddymounu2003@gmail.com"
+}
+for(let x in std){
+    document.write(std[x]+"<br>"
+}*/
+/*for(let x=0;x<5;x=x++)
+{
+    document.write("3*x=")
+    document.write(3*x+"<br>")
+}*/
+let age=25
+if(age>=18){
+    document.write("your age is greater than 18"+"<br>")
+    document.write("your age is="+age)
+}
+else{
+    document.write("your age is lessthan 18"+"<br>")
+    document.write("your age is="+age)
+}
+
+
+
